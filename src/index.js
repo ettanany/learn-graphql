8e4f70522b0567cdbf418601c5447fe717b8efac
Ahmed Et-tanany <ahmed.ettanany@gmail.com>
2019-12-26 00:43:53 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import { GraphQLServer } from 'graphql-yoga';
+import gql from 'graphql-tag';
+
+// Type definitions (schema)
+const typeDefs = gql`
+  type Query {
+    me: User!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    me: () => ({
+      id: '123abc',
+      name: 'John',
+      email: 'john@example.com',
+      age: 26,
+    }),
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(({ port }) => {
+  console.log(`🚀 Server ready at http://localhost:${port}`);
+});
